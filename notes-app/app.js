4512dc7ae78d66237b150d6358b23b9a7f8ab38f
arigatopix <teeruch.k@gmail.com>
2019-05-24 13:07:02 +0700

    :fire: assignemnt getNotes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,6 @@
+// import anather files
 const utils = require('./utils');
+const getNotes = require('./notes');
 
 // จะเข้าไปใช้ variable ใน utils.js ไม่ได้ ต้อง export module  จาก module ก่อน
 console.log(utils.name);
@@ -6,3 +8,7 @@ console.log(utils.name);
 // import and use function
 const sum = utils.add(2,5);
 console.log(sum);
+
+// assigment get notes from notes.js
+const msg = getNotes();
+console.log(msg);

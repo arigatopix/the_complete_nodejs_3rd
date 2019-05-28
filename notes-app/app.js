22c1709fb6cd8d7040a47bb0a983b455b21cb419
arigato <teeruch.k@gmail.com>
2019-05-28 20:40:34 +0700

    npm install validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,20 @@
-// import anather files
+// import package
+const validator = require('validator');
+// import anathor files
 const utils = require('./utils');
 const getNotes = require('./notes');
 
-// จะเข้าไปใช้ variable ใน utils.js ไม่ได้ ต้อง export module  จาก module ก่อน
+// * จะเข้าไปใช้ variable ใน utils.js ไม่ได้ ต้อง export module  จาก module ก่อน
 console.log(utils.name);
 
-// import and use function
-const sum = utils.add(2,5);
+// * import and use function
+const sum = utils.add(2, 5);
 console.log(sum);
 
-// assigment get notes from notes.js
+// * assigment get notes from notes.js
 const msg = getNotes();
 console.log(msg);
+
+// * import package validator
+console.log(validator.isEmail('bas@gmail.com'));
+console.log(validator.isURL('bit/tamsreport'));

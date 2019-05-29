b2f5607ac7eb360426619593a96f9ed5f0292b77
arigato <teeruch.k@gmail.com>
2019-05-29 21:56:40 +0700

    :fire: addNotes to notes.json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,104 +1,71 @@
 // import package
-const validator = require("validator");
-const chalk = require("chalk");
-const yargs = require("yargs");
+const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
 // import another files
-// const utils = require("./utils");
-// const getNotes = require("./notes");
-
-// * จะเข้าไปใช้ variable ใน utils.js ไม่ได้ ต้อง export module  จาก module ก่อน
-// console.log(utils.name);
-
-// * import and use function
-// const sum = utils.add(2, 5);
-// console.log(sum);
-
-// * assigment get notes from notes.js
-// const msg = getNotes();
-// console.log(msg);
-
-// * import package validator
-// console.log(validator.isEmail("bas@gmail.com"));
-// console.log(validator.isURL("bit/tamsreport"));
-
-// * import chalk package library
-// console.log(chalk.blue.inverse.bold("Sucess!"));
-// console.log(chalk.red.inverse.bold("Error!"));
-// console.log(chalk.bold.bgCyan("Pastel"));
-
-// arg vector รับค่า input จาก command line
-// console.log(process.argv); // ได้ array dir ของ node.js และได้รับค่าจาก cmd
-
-// * input command
-// const command = process.argv[2];
-
-// if (command === "add") {
-//   console.log("Adding note!");
-// } else if (command === "remove") {
-//   console.log("Removing note!");
-// }
+const notes = require('./notes');
 
 // * Work with yargs รับ input จาก command line
 
 // Customize yargs version
 // จะขึ้นใน --help ว่าสามารถเรียกใช้งานจาก cml คำสั่งอะไรได้บ้าง
-yargs.version("1.1.0");
+yargs.version('1.1.0');
 
 // * Create Command นอกเหนือจาก default สำหรับรับคำสั่งจาก command line
 
 // create add command
 yargs.command({
-  command: "add",
-  describe: "Add a new note",
+  command: 'add',
+  describe: 'Add a new note',
   builder: {
     // option เอาไว้ระบุการรับค่าจาก command
     title: {
       // บอกว่า option นี้คืออะไร
-      describe: "Note title",
+      describe: 'Note title',
       // ตั้งค่าให้กรอก command line เข้ามา ถ้าไม่มี option นี้ตอนสั่ง command ก็จะ error
       demandOption: true,
       // type ว่าให้ใส่ string เท่านั้น
-      type: "string"
+      type: 'string'
     },
     body: {
       // Challenge
-      describe: "Note body",
+      describe: 'Note body',
       demandOption: true,
-      type: "string"
+      type: 'string'
     }
   },
   handler: function(argv) {
     // แสดงผลข้อความเมื่อเรียกใช้ command
-    console.log("Title " + argv.title); // แสดง title
-    console.log("Body " + argv.body); // แสดง body
+    // เก็บข้อมูลใน JSON file
+    notes.addNote(argv.title, argv.body);
   }
 });
 
 // crate 'remove' command
 yargs.command({
-  command: "remove",
-  describe: "Remove a note",
+  command: 'remove',
+  describe: 'Remove a note',
   handler: function() {
-    console.log("Removing a note!");
+    console.log('Removing a note!');
   }
 });
 
 // create 'list' command
 yargs.command({
-  command: "list",
-  describe: "list your notes",
+  command: 'list',
+  describe: 'list your notes',
   handler: function() {
-    console.log("Listing out all note");
+    console.log('Listing out all note');
   }
 });
 
 // create 'read' command
 yargs.command({
-  command: "read",
-  describe: "Read a note",
+  command: 'read',
+  describe: 'Read a note',
   handler: function() {
-    console.log("Reading a note");
+    console.log('Reading a note');
   }
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,71 @@
-const getNotes = () => {
-  return 'Your notes ...'
-}
+const fs = require('fs');
 
-module.exports = getNotes;
+const getNotes = function() {
+  return 'Your notes ...';
+};
+
+const addNote = function(title, body) {
+  // รับค่าจาก app.js แล้วเพิ่มเข้า object
+
+  // เรียกข้อมูลเก่าจาก notes.json ได้ object แล้ว
+  const notes = loadNotes();
+
+  // filter title, body ที่ซ้ำกันออก
+  const duplicateNotes = notes.filter(function(note) {
+    // notes.title เทียบกับ title ที่รับมา เก็บเฉพาะที่ไม่ซ้ำ ถ้า ture คือซ้ำ โดยเก็บเป็น array ใหม่
+    // โดยปกติเราจะ filter ออกโดยเอา false แต่อันนี้อยากเก็บ true ไปเช็ค
+    return note.title === title;
+  });
+
+  // check
+  // console.log(duplicateNotes);
+
+  if (duplicateNotes === 0) {
+    // ถ้าไม่มีข้อมูลซ้ำ
+
+    // * เพิ่ม object ใหม่ให้ object เดิม * ไม่ถูก overwrite เพราะใช้ push ไปทั้ง object แล้วค่อยเขียน notes.json ใหม่
+    notes.push({
+      title: title,
+      body: body
+    });
+
+    // save to JSON
+    saveNotes(notes);
+
+    console.log('New notes added!!');
+  } else {
+    console.log('dNote title taken!');
+  }
+};
+
+const saveNotes = function(notes) {
+  // save new notes object to JSON
+  const dataJSON = JSON.stringify(notes);
+
+  // writeFile
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = function() {
+  // ERROR handle
+  try {
+    // * ถ้ามีข้อมูลใน notes.json จะ run code นี้
+
+    // load from notes.json ใช้ fs.readFileSync
+    const dataBuffer = fs.readFileSync('notes.json');
+
+    // converse Buffer to JSON (string)
+    const dataJSON = dataBuffer.toString();
+
+    // แปลงเป็น object return ไปใช้ใน function ข้างนอก
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    // ถ้าไม่มีข้อมูลใน notes.json สร้าง empty array
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};

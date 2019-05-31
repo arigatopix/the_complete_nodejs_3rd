6ba0f1e880a1e4631e4c24fed78390f22ed8c5eb
arigato <teeruch.k@gmail.com>
2019-05-31 20:32:12 +0700

    :fire: fetch currently forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,23 @@
-const request = require("request");
-const key = require("./api");
-const coordinate = "15.0792301,100.67687";
+const request = require('request');
+const key = require('./api');
+const coordinate = '15.0792301,100.67687';
 
-const url = `https://api.darksky.net/forecast/${key}/${coordinate}`;
+const url = `https://api.darksky.net/forecast/${key}/${coordinate}?units=si`;
 
-// request ดูใน document
 request({ url: url, json: true }, (error, response) => {
-  console.log(response);
+  // request ดูใน document
+
+  // Parse API เป็น object
+  // const data = JSON.parse(response.body);
+  // ใช้ request package ช่วยแปลง json to object
+  // console.log(response.body.currently);
+
+  // Challenge แสดงผล Temperature และ % ปริมาณฝนน่าจะตก
+  // destructuring
+  const { temperature, precipProbability } = response.body.currently;
+
+  // แสดง Temp และ % ฝนตก
+  console.log(
+    `It is currently ${temperature} degrees out . There is a ${precipProbability}% change of rain.`
+  );
 });
